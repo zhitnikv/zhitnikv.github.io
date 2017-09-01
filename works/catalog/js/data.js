27cbc573e8d5b5f09198c0df7d32e74731b70a23
zhitnikv <vitaliyzhitnik22@gmail.com>
2017-09-01 05:03:07 +0300

    Add files via upload

---
+++ works/catalog/js/data.js
@@ -0,0 +1,72 @@
+var DATA = [
+	{
+		id: 1,
+		artist: "Ирина Аллегрова",
+		songName: "Угонщица"
+
+	}, {
+		id: 2,
+		artist: "Макс Барских",
+		songName: "Подруга-ночь"
+	}, {
+		id: 3,
+		artist: "Григорий Лепс",
+		songName: "Рюмка водки"
+	}, {
+		id: 4,
+		artist: "Ирина Аллегрова",
+		songName: "Императрица"
+	}, {
+		id: 5,
+		artist: "Ленинград",
+		songName: "WWW"
+	},{
+		id: 6,
+		artist: "Бумбокс",
+		songName: "Полiна"
+
+	}, {
+		id: 7,
+		artist: "Ани Лорак",
+		songName: "Песня Оксаны"
+	}, {
+		id: 8,
+		artist: "Григорий Лепс",
+		songName: "Ну и что"
+	}, {
+		id: 9,
+		artist: "Ирина Аллегрова",
+		songName: "Бабы-стервы"
+	}, {
+		id: 10,
+		artist: "Rage Against the machine",
+		songName: "Wake up!"
+	}, {
+		id: 11,
+		artist: "Ани Лорак",
+		songName: "Я стану морем"
+
+	}, {
+		id: 12,
+		artist: "Макс Барских",
+		songName: "Я хочу танцевать"
+	}, {
+		id: 13,
+		artist: "Браво",
+		songName: "Я то что надо"
+	}, {
+		id: 14,
+		artist: "Браво",
+		songName: "Вася"
+	}, {
+		id: 15,
+		artist: "Ленинград",
+		songName: "День Рождения"
+	},{
+		id: 16,
+		artist: "Ирина Аллегрова",
+		songName: "С Днем Рождения"
+
+	}
+
+];
